ed54803b18cfba8a0c7e30a09c9607b194a5d131
Divine Orji <dpkreativ@gmail.com>
2022-06-19 08:24:20 +0100

    Create `OptimizedImage` component

---
+++ src/components/OptimizedImage/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { AdvancedImage } from '@cloudinary/react';
+import { Cloudinary } from '@cloudinary/url-gen';
+import { scale } from '@cloudinary/url-gen/actions/resize';
+
+export default function OptimizedImage({ publicId }) {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'dpkreativ',
+    },
+  });
+
+  const myImage = cld.image(`${publicId}`);
+
+  myImage.quality('auto').format('auto').resize(scale().width(480));
+
+  return (
+    <div>
+      <AdvancedImage cldImg={myImage} />
+    </div>
+  );
+}
